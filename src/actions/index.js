6223cdd6a46943bbf1295f5c9bab09ad217fd826
DanielKell <danielkell.ubc@gmail.com>
2017-09-30 16:21:50 -0700

    Set up a fetch weather action and linked a url request to the payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,20 @@
+import axios from 'axios';
+//axios is a smaller, simpler way to get AJAX requests than JQuery
+
 const API_KEY = "03523f555745feab8f58016bab98d180";
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`; 
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+//This helps keep our action types consistent between our action creators
+//and action reducers. Just for consistency/bugproofing!
+
+export function fetchWeather(city) {
+    const url = `${ROOL_URL}&q=${city},ca`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request //Add'l data that describes the action
+    };
+}
